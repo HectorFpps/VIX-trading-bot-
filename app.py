370b43ac7dc0a3d658cf589145d4ce7b43e238bd
HectorFpps <71526987+HectorFpps@users.noreply.github.com>
2022-01-12 19:47:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from datetime import datetime
 # “1m”, “2m”, “5m”, “15m”, “30m”, “60m”, “90m”, “1h”, “1d”, “5d”, “1wk”, “1mo”, “3mo”
 
 #User input
-chartPeriod = "800d"
-interval = "90m"
+chartPeriod = "700d"
+interval = "1h"
 rsiPeriod = 14
 buyLimit = 50
 sellLimit = 65
