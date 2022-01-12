9156ddc0166952f21df70f159d86af3303504479
HectorFpps <71526987+HectorFpps@users.noreply.github.com>
2022-01-12 19:46:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import pandas_ta as ta
 import streamlit as st
 from datetime import datetime
 
+# “1m”, “2m”, “5m”, “15m”, “30m”, “60m”, “90m”, “1h”, “1d”, “5d”, “1wk”, “1mo”, “3mo”
+
 #User input
 chartPeriod = "800d"
-interval = "1h"
+interval = "90m"
 rsiPeriod = 14
 buyLimit = 50
 sellLimit = 65
