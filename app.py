87cb4c769e938911eb394e8e0ac11fce98db8b82
HectorFpps <71526987+HectorFpps@users.noreply.github.com>
2022-01-12 18:19:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ balanceTrack = []
 for i in range(len(prices)):
 
     if not inPosition:
-        if float(prices[i]) < 19:
+        if float(prices[i]) < 20:
             buyPrice = prices[i]
             inPosition = True
 
