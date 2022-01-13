54b98a03347f84e605a330c6cc2d7a27646b5df7
dberweger2017 <94843489+dberweger2017@users.noreply.github.com>
2022-01-13 23:55:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -155,8 +155,8 @@ def trade():
     inPosition = False
     balance = 10000
     
-    for i in range(len(prices)-testingRange,len(prices)-1):
-        pricesInRange += prices[i]
+    for i in range(len(prices)-testingRange,len(prices)):
+        pricesInRange += [prices[i]]
         if not inPosition:
             if(rsi[i] < rsiBuy) and (prices[i] < (ema1b[i]-buyUnderNumber1)) and (prices[i] < (ema2b[i]-buyUnderNumber2)):
                 buyPrice = prices[i]
