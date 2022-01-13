a71198c11527a8c48cb1baa2896c9dc69deaae02
HectorFpps <71526987+HectorFpps@users.noreply.github.com>
2022-01-13 05:40:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ sells = []
 
 #Market data
 df = pd.DataFrame()
-df = df.ta.ticker("^VIX", period=chartPeriod, interval=interval)
+df = df.ta.ticker("VXX", period=chartPeriod, interval=interval)
 prices = []
 rsi = []
 ema1 = []
