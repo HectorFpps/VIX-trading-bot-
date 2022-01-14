4541bee67e1f7061ad37d1835a84d68ca6696850
dberweger2017 <94843489+dberweger2017@users.noreply.github.com>
2022-01-14 01:23:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # “1m”, “2m”, “5m”, “15m”, “30m”, “60m”, “90m”, “1h”, “1d”, “5d”, “1wk”, “1mo”, “3mo”
 
 #User input
-chartPeriod = "1d"
+chartPeriod = "5d"
 interval = "1h"
 rsiPeriod = 14
 rsiBuy = 50
