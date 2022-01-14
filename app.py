a884c372466d0e820e5ccc8b0a9c4fccb79371e2
dberweger2017 <94843489+dberweger2017@users.noreply.github.com>
2022-01-14 01:27:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import yfinance as yf
 import pandas as pd
 import pandas_ta as ta
 import streamlit as st
-from datetime import datetime
 
 # “1m”, “2m”, “5m”, “15m”, “30m”, “60m”, “90m”, “1h”, “1d”, “5d”, “1wk”, “1mo”, “3mo”
 
